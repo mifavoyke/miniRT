3122cf4157ca390f9e303fc21f0b28d05ba5f30f
zuzanapiarova <zuzana.piarova1@gmail.com>
2025-04-29 23:07:03 +0200

    transformations probably done, test more

--- src/utils/hooks.c
+++ src/utils/hooks.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zpiarova <zpiarova@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zuzanapiarova <zuzanapiarova@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 14:13:05 by zpiarova          #+#    #+#             */
-/*   Updated: 2025/04/29 18:10:12 by zpiarova         ###   ########.fr       */
+/*   Updated: 2025/04/29 23:04:34 by zuzanapiaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,35 @@
 //      | 0   0   0  Tz | - [ Rz  Fz  Uz  *Tz ]
 //      | 0   0   0   1 | - [  0   0   0    1 ]
 // the elements with * are the ones to be updated by the rotation
-void move(t_minirt *minirt, float *coord, float translation)
+// void move(t_minirt *minirt, float *coord, float translation)
+// {
+//     *coord += translation;
+//     minirt->scene->Tm = find_transformation_matrix(minirt->scene->c);
+//     print_coord(minirt->scene->c.point);
+//     if (generate_image(minirt) == ERROR)
+//     {
+//         cleanup(minirt);
+//         exit(ERROR);
+//     }
+// }
+
+// this moves camera relative to its local axis, not relative to the world axis 
+// direction is the direction of movement in local camera space - forward, up, or right
+void move(t_minirt *minirt, t_coord direction, float translation)
 {
-    *coord += translation;
-    minirt->scene->Tm = find_transformation_matrix(minirt->scene->c);
-    print_coord(minirt->scene->c.point);
+    t_camera *cam;
+    t_matrix *Tm;
+
+    cam = &(minirt->scene->c);
+    Tm = &(minirt->scene->Tm);
+    cam->point.x += direction.x * translation;
+    cam->point.y += direction.y * translation;
+    cam->point.z += direction.z * translation;
+    // ✅ Rebuild the transformation matrix
+    Tm->Tr.x = cam->point.x;
+    Tm->Tr.y = cam->point.y;
+    Tm->Tr.z = cam->point.z;
+    // Re-render
     if (generate_image(minirt) == ERROR)
     {
         cleanup(minirt);
@@ -31,22 +55,24 @@ void move(t_minirt *minirt, float *coord, float translation)
     }
 }
 
+// I have a camera positioned in 3D space and oriented by a transformation matrix from
+// a default position (0, 0, 0) and default forward vector (0, 1, 0)
+
 // Rotate a vector `v` around `axis` by `angle` radians - Rodriguez's formula
 t_coord rotate_vector_around_axis(t_coord v, t_coord axis, float angle)
 {
     t_coord result;
     float c = cosf(angle);
-    float s = sinf(angle);
+    float s = -sinf(angle); // set to minus as in left-handed system the forwrd vector is minus the cross product of right x up
 
     // Normalize the axis
     normalize(&axis);
-
     // Rodrigues' rotation formula
     result.x = v.x * c + (1 - c) * (axis.x * (axis.x * v.x + axis.y * v.y + axis.z * v.z)) + s * (axis.y * v.z - axis.z * v.y);
     result.y = v.y * c + (1 - c) * (axis.y * (axis.x * v.x + axis.y * v.y + axis.z * v.z)) + s * (axis.z * v.x - axis.x * v.z);
     result.z = v.z * c + (1 - c) * (axis.z * (axis.x * v.x + axis.y * v.y + axis.z * v.z)) + s * (axis.x * v.y - axis.y * v.x);
 
-    return result;
+    return (result);
 }
 
 // !!!!!!!!!!!!!!!!!!!!
@@ -69,35 +95,12 @@ void rotate_x(t_minirt *minirt, t_coord *original_vector, float angle)
     t_matrix *Tm;
     
     Tm = &(minirt->scene->Tm);
-    t_coord old_f = Tm->F;
-    t_coord old_u = Tm->U;
-    t_coord axis = Tm->R; // Local X axis
-
-    float c = cosf(angle);
-    float s = sinf(angle);
-
-    // Rotate Forward vector around Right
-    Tm->F = add_vectors(
-                multiply_vector_by_constant(old_f, c),
-                multiply_vector_by_constant(get_cross_product(axis, old_f), s)
-            );
-
-    // Rotate Up vector around Right
-    Tm->U = add_vectors(
-                multiply_vector_by_constant(old_u, c),
-                multiply_vector_by_constant(get_cross_product(axis, old_u), s)
-            );
-
+    Tm->F = rotate_vector_around_axis(Tm->F, Tm->R, angle);
+    Tm->U = rotate_vector_around_axis(Tm->U, Tm->R, angle);
     normalize(&Tm->F);
     normalize(&Tm->U);
     Tm->R = get_cross_product(Tm->F, Tm->U);
     normalize(&Tm->R);
-    // Tm->F = rotate_vector_around_axis(Tm->F, Tm->R, angle);
-    // Tm->U = rotate_vector_around_axis(Tm->U, Tm->R, angle);
-    // normalize(&Tm->F);
-    // normalize(&Tm->U);
-    // Tm->R = get_cross_product(Tm->F, Tm->U);
-    // normalize(&Tm->R);
     *original_vector = Tm->F;
     if (generate_image(minirt) == ERROR)
     {
@@ -119,35 +122,12 @@ void rotate_y(t_minirt *minirt, t_coord *original_vector, float angle)
     t_matrix *Tm;
     
     Tm = &(minirt->scene->Tm);
-    t_coord old_r = Tm->R;
-    t_coord old_u = Tm->U;
-    t_coord axis = Tm->F; // Local Y axis (Forward)
-
-    float c = cosf(angle);
-    float s = sinf(angle);
-
-    // Rotate Right vector around Forward
-    Tm->R = add_vectors(
-                multiply_vector_by_constant(old_r, c),
-                multiply_vector_by_constant(get_cross_product(axis, old_r), s)
-            );
-
-    // Rotate Up vector around Forward
-    Tm->U = add_vectors(
-                multiply_vector_by_constant(old_u, c),
-                multiply_vector_by_constant(get_cross_product(axis, old_u), s)
-            );
-
+    Tm->R = rotate_vector_around_axis(Tm->R, Tm->F, angle);
+    Tm->U = rotate_vector_around_axis(Tm->U, Tm->F, angle);
     normalize(&Tm->R);
     normalize(&Tm->U);
-    Tm->F = get_cross_product(Tm->U, Tm->R);
+    Tm->F = get_cross_product(Tm->R, Tm->U);
     normalize(&Tm->F);
-    // Tm->R = rotate_vector_around_axis(Tm->R, Tm->F, angle);
-    // Tm->U = rotate_vector_around_axis(Tm->U, Tm->F, angle);
-    // normalize(&Tm->R);
-    // normalize(&Tm->U);
-    // Tm->F = get_cross_product(Tm->R, Tm->U);
-    // normalize(&Tm->F);
     *original_vector = Tm->F;
     if (generate_image(minirt) == ERROR)
     {
@@ -169,36 +149,12 @@ void rotate_z(t_minirt *minirt, t_coord *original_vector, float angle)
     t_matrix *Tm;
     
     Tm = &(minirt->scene->Tm);
-    
-    t_coord old_r = Tm->R;
-    t_coord old_f = Tm->F;
-    t_coord axis = Tm->U; // Local Z axis (Up vector)
-    float c = cosf(angle);
-    float s = sinf(angle);
-
-    // Rotate Right vector around Up
-    Tm->R = add_vectors(
-                multiply_vector_by_constant(old_r, c),
-                multiply_vector_by_constant(get_cross_product(axis, old_r), s)
-            );
-
-    // Rotate Forward vector around Up
-    Tm->F = add_vectors(
-                multiply_vector_by_constant(old_f, c),
-                multiply_vector_by_constant(get_cross_product(axis, old_f), s)
-            );
-
-    // Renormalize everything to prevent drift
+    Tm->R = rotate_vector_around_axis(Tm->R, Tm->U, angle);
+    Tm->F = rotate_vector_around_axis(Tm->F, Tm->U, angle);
     normalize(&Tm->R);
     normalize(&Tm->F);
-    Tm->U = get_cross_product(Tm->F, Tm->R);
+    Tm->U = get_cross_product(Tm->R, Tm->F);
     normalize(&Tm->U);
-    // Tm->R = rotate_vector_around_axis(Tm->R, Tm->U, angle);
-    // Tm->F = rotate_vector_around_axis(Tm->F, Tm->U, angle);
-    // normalize(&Tm->R);
-    // normalize(&Tm->F);
-    // Tm->U = get_cross_product(Tm->R, Tm->F);
-    // normalize(&Tm->U);
     *original_vector = Tm->F;
     if (generate_image(minirt) == ERROR)
     {
@@ -216,26 +172,34 @@ void ft_hook(void *param)
         mlx_close_window(minirt->mlx);
     // changes position of the camera
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_D))
-        move(minirt, &minirt->scene->c.point.x, TRANSLATION);
+        move(minirt, minirt->scene->Tm.R, TRANSLATION);
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_A))
-        move(minirt, &minirt->scene->c.point.x, -TRANSLATION);
+        move(minirt, minirt->scene->Tm.R, -TRANSLATION);
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_W))
-        move(minirt, &minirt->scene->c.point.z, TRANSLATION);
+        move(minirt, minirt->scene->Tm.U, TRANSLATION);
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_S))
-        move(minirt, &minirt->scene->c.point.z, -TRANSLATION);
+        move(minirt, minirt->scene->Tm.U, -TRANSLATION);
+    // if (mlx_is_key_down(minirt->mlx, MLX_KEY_D))
+    //     move(minirt, &minirt->scene->c.point.x, TRANSLATION);
+    // if (mlx_is_key_down(minirt->mlx, MLX_KEY_A))
+    //     move(minirt, &minirt->scene->c.point.x, -TRANSLATION);
+    // if (mlx_is_key_down(minirt->mlx, MLX_KEY_W))
+    //     move(minirt, &minirt->scene->c.point.z, TRANSLATION);
+    // if (mlx_is_key_down(minirt->mlx, MLX_KEY_S))
+    //     move(minirt, &minirt->scene->c.point.z, -TRANSLATION);
     // DIRECTIONAL VECTOR
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_UP))
-        rotate_x(minirt, &minirt->scene->c.vector, ANGLE_RADIAN);
-    if (mlx_is_key_down(minirt->mlx, MLX_KEY_DOWN))
         rotate_x(minirt, &minirt->scene->c.vector, -ANGLE_RADIAN);
+    if (mlx_is_key_down(minirt->mlx, MLX_KEY_DOWN))
+        rotate_x(minirt, &minirt->scene->c.vector, ANGLE_RADIAN);
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_K))
-        rotate_y(minirt, &minirt->scene->c.vector, ANGLE_RADIAN);
-    if (mlx_is_key_down(minirt->mlx, MLX_KEY_J))
         rotate_y(minirt, &minirt->scene->c.vector, -ANGLE_RADIAN);
+    if (mlx_is_key_down(minirt->mlx, MLX_KEY_J))
+        rotate_y(minirt, &minirt->scene->c.vector, ANGLE_RADIAN);
     if (mlx_is_key_down(minirt->mlx, MLX_KEY_LEFT))
-        rotate_z(minirt, &minirt->scene->c.vector, ANGLE_RADIAN);
-    if (mlx_is_key_down(minirt->mlx, MLX_KEY_RIGHT))
         rotate_z(minirt, &minirt->scene->c.vector, -ANGLE_RADIAN);
+    if (mlx_is_key_down(minirt->mlx, MLX_KEY_RIGHT))
+        rotate_z(minirt, &minirt->scene->c.vector, ANGLE_RADIAN);
 }
 
 void scroll_zoom(double xdelta, double ydelta, void *param)
@@ -245,9 +209,9 @@ void scroll_zoom(double xdelta, double ydelta, void *param)
 
     minirt = (t_minirt *)param;
     if (ydelta > 0)
-        move(minirt, &minirt->scene->c.point.y, ZOOM);
+        move(minirt, minirt->scene->Tm.F, ZOOM);
     else if (ydelta < 0)
-        move(minirt, &minirt->scene->c.point.y, -ZOOM);
+        move(minirt, minirt->scene->Tm.F, -ZOOM);
 }
 
 void resize_hook(int width, int height, void *param)
